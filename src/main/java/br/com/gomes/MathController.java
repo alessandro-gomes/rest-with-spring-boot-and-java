1b9dddb468ecfd47e5ecb3d179d05e6577b9ee08
alessandro-gomes <alessandro_gomes@outlook.com.br>
2023-12-31 10:03:50 -0300

    alteração de código na classe MathController, feito validações de Strings recebidas, conversão de String para Double e criação de método para retornar resultado da soma.

--- src/main/java/br/com/gomes/MathController.java
+++ src/main/java/br/com/gomes/MathController.java
@@ -1,7 +1,5 @@
 package br.com.gomes;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,11 +8,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MathController{
 
-	private final AtomicLong counter = new AtomicLong();
 	
 	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
-	public Double sum(@PathVariable(value = "numberOne") Double numberOne , @PathVariable(value = "numberTwo") Double numberTwo) {
-		return numberOne + numberTwo;
+	public Double sum(@PathVariable(value = "numberOne") String numberOne , @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+		
+		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new Exception(); 
+		}
+		
+		return convertToDouble(numberOne) + convertToDouble(numberTwo);
+	}
+
+	private boolean isNumeric(String strNumber) {
+		if (strNumber == null) return false;
+		String number = strNumber.replaceAll(",", ".");
+		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
 	}
 	
+	private Double convertToDouble(String strNumber) {
+		if (strNumber == null) return 0D; 
+			// BR 10,25  US 10.25
+			String number = strNumber.replaceAll(",", ".");
+			if (isNumeric(number)) return Double.parseDouble(number);
+			return 0D;
+	}
+
 }
